c13eaf4caeb91ba9dc3f26aa28024515c36e3e66
Ananthan-Rock <anan.intech@gmail.com>
2019-11-08 12:18:42 +0530

    parameterized example

---
+++ CMakeFiles/ParameterizedTestExamples.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ParameterizedTestExamples.dir/test/main.cpp.o"
+  "CMakeFiles/ParameterizedTestExamples.dir/src/LeapYear.cpp.o"
+  "CMakeFiles/ParameterizedTestExamples.dir/test/LeapYearStandaloneTests.cpp.o"
+  "CMakeFiles/ParameterizedTestExamples.dir/test/LeapYearFixtureTests.cpp.o"
+  "CMakeFiles/ParameterizedTestExamples.dir/test/LeapYearIterationTest.cpp.o"
+  "CMakeFiles/ParameterizedTestExamples.dir/test/LeapYearParameterizedTestFixture.cpp.o"
+  "CMakeFiles/ParameterizedTestExamples.dir/test/LeapYearFixtureCombinedWithParameterizedTests.cpp.o"
+  "CMakeFiles/ParameterizedTestExamples.dir/test/LeapYearMultipleParametersTests.cpp.o"
+  "ParameterizedTestExamples.pdb"
+  "ParameterizedTestExamples"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ParameterizedTestExamples.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
