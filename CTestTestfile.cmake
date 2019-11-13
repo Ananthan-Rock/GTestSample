ea2979381eef61efb6c1dc1080b649b7c99c175b
apoongavanam <Ananthan.Poongavanam@harman.com>
2019-11-13 18:10:24 +0530

    modified code

--- CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jenkins/Documents/GTestSample
-# Build directory: /home/jenkins/Documents/GTestSample
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(ParameterizedTestExamples "/home/jenkins/Documents/GTestSample/ParameterizedTestExamples")
